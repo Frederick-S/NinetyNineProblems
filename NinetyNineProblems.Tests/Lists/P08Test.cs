1f27279c954661091afd45876a9a10aa364c530d
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-12 11:45:13 +0800

    Add p08

---
+++ NinetyNineProblems.Tests/Lists/P08Test.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using NinetyNineProblems.Lists;
+using Xunit;
+
+namespace NinetyNineProblems.Tests.Lists
+{
+    public class P08Test
+    {
+        [Fact]
+        public void ShouldReturnCompressedList()
+        {
+            var list = "aaaabccaadeeee".ToCharArray()
+                .OfType<char>()
+                .Select(x => x.ToString())
+                .ToList();
+
+            Assert.Equal(new List<string> { "a", "b", "c", "a", "d", "e" }, P08.Compress(list));
+        }
+    }
+}
---
+++ NinetyNineProblems/Lists/P08.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NinetyNineProblems.Lists
+{
+    public class P08
+    {
+        public static List<T> Compress<T>(List<T> list)
+        {
+            return CompressInternal(list, 0);
+        }
+
+        public static List<T> CompressInternal<T>(List<T> list, int index)
+        {
+            if (index == list.Count)
+            {
+                return new List<T>();
+            }
+            else if (index != 0 && list[index].Equals(list[index - 1]))
+            {
+                return CompressInternal(list, index + 1);
+            }
+            else
+            {
+                return new List<T> { list[index] }.Concat(CompressInternal(list, index + 1)).ToList();
+            }
+        }
+    }
+}
