b942a9ae2aad1bb610b4650ea3f8da164d36e1bf
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-16 13:57:55 +0800

    Add p09

---
+++ NinetyNineProblems.Tests/Lists/P09Test.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using NinetyNineProblems.Lists;
+using Xunit;
+
+namespace NinetyNineProblems.Tests.Lists
+{
+    public class P09Test
+    {
+        [Fact]
+        public void ShouldReturnPackedList()
+        {
+            var list = new List<char> { 'a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e' };
+            var expectedList = new List<List<char>>
+            {
+                 new List<char> { 'a', 'a', 'a', 'a' },
+                 new List<char> { 'b' },
+                 new List<char> { 'c', 'c' },
+                 new List<char> { 'a', 'a' },
+                 new List<char> { 'd' },
+                 new List<char> { 'e', 'e', 'e', 'e' },
+            };
+
+            Assert.Equal(expectedList, P09.Pack(list));
+        }
+    }
+}
---
+++ NinetyNineProblems/Lists/P09.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NinetyNineProblems.Lists
+{
+    public class P09
+    {
+        public static List<List<T>> Pack<T>(List<T> list)
+        {
+            return PackInternal(list, 0);
+        }
+
+        public static List<List<T>> PackInternal<T>(List<T> list, int index)
+        {
+            if (index == list.Count)
+            {
+                return new List<List<T>>();
+            }
+            else if (index != list.Count - 1 && list[index].Equals(list[index + 1]))
+            {
+                var left = PackInternal(list, index + 1);
+
+                return Enumerable.Range(0, left.Count).Select(i =>
+                    {
+                        return i == 0 ? new List<T> { list[index] }.Concat(left[0]).ToList() : left[i];
+                    }).ToList();
+            }
+            else
+            {
+                return new List<List<T>> { new List<T> { list[index] } }.Concat(PackInternal(list, index + 1)).ToList();
+            }
+        }
+    }
+}
