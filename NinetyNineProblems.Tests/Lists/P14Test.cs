83213b7c6a3b2dac4508e5586a2ea64b444d3308
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-19 11:52:07 +0800

    Add p14

---
+++ NinetyNineProblems.Tests/Lists/P14Test.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using NinetyNineProblems.Lists;
+using Xunit;
+
+namespace NinetyNineProblems.Tests.Lists
+{
+    public class P14Test
+    {
+        [Fact]
+        public void ShouldReturnDuplicatedList()
+        {
+            var list = new List<char> { 'a', 'b', 'c', 'c', 'd' };
+            var expectedList = new List<char> { 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'c', 'd', 'd' };
+
+            Assert.Equal(expectedList, P14.Dupli(list));
+        }
+    }
+}
---
+++ NinetyNineProblems/Lists/P14.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NinetyNineProblems.Lists
+{
+    public class P14
+    {
+        public static List<T> Dupli<T>(List<T> list)
+        {
+            return list.SelectMany(x => Enumerable.Repeat(x, 2)).ToList();
+        }
+    }
+}
