2c1e04d93c5a0b3ef66718467c032416088ce2ab
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-20 09:25:04 +0800

    Add p18

---
+++ NinetyNineProblems.Tests/Lists/P18Test.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using NinetyNineProblems.Lists;
+using Xunit;
+
+namespace NinetyNineProblems.Tests.Lists
+{
+    public class P18Test
+    {
+        [Fact]
+        public void ShouldReturnSlicedList()
+        {
+            var list = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k' };
+            var expectedList = new List<char> { 'c', 'd', 'e', 'f', 'g' };
+
+            Assert.Equal(expectedList, P18.Slice(list, 3, 7));
+        }
+    }
+}
