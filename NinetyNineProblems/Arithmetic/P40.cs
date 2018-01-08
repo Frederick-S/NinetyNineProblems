e5939dde7f2adbaee772a671f7f07c2390202acc
Frederick-S <mao_xiaodan@hotmail.com>
2018-01-08 21:30:16 +0800

    Update p40

--- NinetyNineProblems/Arithmetic/P40.cs
+++ NinetyNineProblems/Arithmetic/P40.cs
@@ -12,21 +12,11 @@ namespace NinetyNineProblems.Arithmetic
             Debug.Assert(n > 2 && n % 2 == 0, "n must be a positive even number greater than 2");
 
             List<int> primes = Enumerable.Range(3, n - 3).Where(i => P31.IsPrime(i)).ToList();
-            List<int> primes2 = primes.Select(i => i).ToList();
 
             HashSet<int> primesSet1 = new HashSet<int>(primes);
-            HashSet<int> primesSet2 = new HashSet<int>(primes2);
-            List<Tuple<int, int>> groups = new List<Tuple<int, int>>();
+            HashSet<int> primesSet2 = new HashSet<int>(primes);
 
-            foreach (int number in primesSet1)
-            {
-                if (primesSet2.Contains(n - number) && number <= n - number)
-                {
-                    groups.Add(Tuple.Create(number, n - number));
-                }
-            }
-
-            return groups;
+            return primesSet1.Where(i => primesSet2.Contains(n - i) && i <= n - i).Select(i => Tuple.Create(i, n - i)).ToList();
         }
     }
 }
